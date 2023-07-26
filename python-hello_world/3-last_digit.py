ffe2102b0fbe3dce72e9c87c52fcf500b45de9da
Divalier <111007588+Divalier@users.noreply.github.com>
2023-07-26 05:31:59 +0300

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,11 +9,11 @@ if fnum=="-":
     if lnum ==0:
        print(f"Last digit of {number} is {lnum} and is 0")
     else:
-       print(f"Last digit of {number} is -{lnum} aand is less than 6 and not 0")
+       print(f"Last digit of {number} is -{lnum} and is less than 6 and not 0")
 if not fnum=="-":
     if lnum >5:
       print(f"Last digit of {number} is {lnum} and is greater than 5")
     if lnum ==0:
       print(f"Last digit of {number} is {lnum} and is 0")
     if lnum<6 and  not lnum == 0:
-      print(f"Last digit of {number} is {lnum} aand is less than 6 and not 0")
+      print(f"Last digit of {number} is {lnum} and is less than 6 and not 0")
