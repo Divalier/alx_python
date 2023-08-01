af6f8fae9cc0dcc976bb824bdd6d92bdd89779ef
Divalier <111007588+Divalier@users.noreply.github.com>
2023-08-02 02:50:05 +0300

    hh

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,5 @@
 for i in range(0,100):
-    if i<10:
-        print(f"0{i}, ", end=" ")
-    elif i==99:
+    if i==99:
         print("99", end="\n")
     else:
-        print(f"{i}, ", end=" ")
+        print("{:02d}, ".format(i), end="")
---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,3 @@
+for i in range(10):
+    for j in range(i + 1, 10):
+        print(f"{str(i)}{str(j)}," ,end=" ")
