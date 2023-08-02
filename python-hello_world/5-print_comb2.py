0a84eeff04014bd7e6b02202f73754382e8f1d14
Divalier <111007588+Divalier@users.noreply.github.com>
2023-08-02 03:28:40 +0300

    correction

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,5 @@
 for i in range(0,100):
-    if i==99:
-        print("99", end="\n")
+    if (i==99):
+        print('{0}'.format(i), end ="/n")
     else:
         print("{:02d}, ".format(i), end="")
--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        print('{0}{1},'.format(i,j) ,end=" ")
+        if (i==8 and j ==9):
+          print('{0}{1},'.format(i,j) ,end="/n")
+        else:
+            print('{0}{1},'.format(i,j) ,end="/n")
