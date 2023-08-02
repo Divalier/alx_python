ef72755b3a8492ed7fb6d4933f9754f3b4ef8cb3
Divalier <111007588+Divalier@users.noreply.github.com>
2023-08-02 03:33:43 +0300

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if (i==8 and j ==9):
-          print('{0}{1},'.format(i,j) ,end="/n")
+          print('{0}{1},'.format(i,j) ,end="\n")
         else:
-            print('{0}{1},'.format(i,j) ,end="/n")
+            print('{0}{1},'.format(i,j) ,end="")
