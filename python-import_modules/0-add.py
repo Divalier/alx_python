ecbabb803055263572cbb6721c84ea3bc3f13087
Divalier <111007588+Divalier@users.noreply.github.com>
2023-08-16 22:08:36 +0300

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 from add_0 import add
-def add(a, b):
- a= 1
- b = 2
- res = add(a, b)
- print("{} + {} = {}".format(a, b, res))
-add()
+a= 1
+b = 2
+res = add(a, b)
+print("{} + {} = {}".format(a, b, res))
+
