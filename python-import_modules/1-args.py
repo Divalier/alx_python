27cb1cc47b1e6fb00f20aa5006315dfede15cb1f
Divalier <111007588+Divalier@users.noreply.github.com>
2023-08-22 01:04:00 +0300

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,12 +1,16 @@
 import sys
 def main():
-    argv = sys.argv[1:]
-    num_arguments = len(argv)
-    print("Number of argument{}: {}".format('s' if num_arguments != 1 else '',num_arguments), end='')
-    if num_arguments > 0:
+    if __name__ == "__main__":
+     argv = sys.argv[1:]
+     num_arguments = len(argv)
+     if num_arguments != 1:
+        a='s'
+     else:
+        a ='' 
+     print("Number of argument{}".format(a), end='')
+     if num_arguments > 0:
         print(":", end='')
-    print()
-    for i, arg in enumerate(argv, start=1):
+     print()
+     for i, arg in enumerate(argv, start=1):
         print("{}: {}".fomart(i,arg))
-if __name__ == "__main__":
-    main()
+
