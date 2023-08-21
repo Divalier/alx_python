3387f45ebbfe703990ca90f4c7a09cfffd825988
Divalier <111007588+Divalier@users.noreply.github.com>
2023-08-21 22:30:25 +0300

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,24 +1,5 @@
-# 0-add.py
-
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return a + b
-
-if __name__ == "__main__":
-    # Assign values to variables a and b
-    a = 1
-    b = 2
-
-    # Calculate the result of the addition
-    result = add(a, b)
-
-    # Print the formatted output
-    print("{} + {} = {}".format(a,b,result))
+from add_0 import add
+a = 1
+b = 2
+result = add(a, b)
+print("{} + {} = {}".format(a,b,result))
