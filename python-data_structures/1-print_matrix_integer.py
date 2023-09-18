2250b599e8a673e982ce4afc906876e6a846ef96
Divalier <111007588+Divalier@users.noreply.github.com>
2023-09-18 02:36:48 -0700

    Update 1-print_matrix_integer.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,6 @@
+#!usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, num in enumerate(row):
-            if i == len(row) - 1:
-                print("{:d}".format(num))
-            else:
-                print("{:d}".format(num), end=" ")
-    
+        for i, element in enumerate(row):
+            print("{:d}".format(element), end=" " if i < len(row) - 1 else '')
+        print()
