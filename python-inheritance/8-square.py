5d1d37c9aa75019e1a07b0acf0de40d92ef134f8
Divalier <111007588+Divalier@users.noreply.github.com>
2023-10-03 23:53:52 +0200

    Update 8-square.py

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,49 +1,59 @@
-""" 
-Write a class Square that inherits from Rectangle (7-rectangle.py)
+#!/usr/bin/env/python3
 """
+Write a class Square that inherits from Rectangle (7-rectangle.py):
+
+Instantiation with size: def __init__(self, size)::
+size must be private. No getter or setter
+size must be a positive integer, validated by integer_validator
+the area() method must be implemented
+
+"""
+class BaseGeometry:
+    """
+    A geometry class.
+    """
+
+    def area(self):
+       
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """
+    A geometry class.
+    """
+
+    def __init__(self, width, height):
+        
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+       
+        return self.__width * self.__height
+
+    def __str__(self):
+        
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
 class Square(Rectangle):
     """
-    A class representing a Square shape.
-
-    Attributes
-    ----------
-    __size : int
-        The size of the Square object.
-
-    Methods
-    -------
-    area():
-        Returns the area of the Square object.
+    A geometry class.
     """
 
     def __init__(self, size):
-        """
-        Constructs and initializes a new Square object with the
-        specified size value.
-
-        Parameters
-        ----------
-        size : int
-            The size of the Square object.
-
-        Raises
-        ------
-        TypeError
-            If size is not an integer.
-        ValueError
-            If size is less than or equal to 0.
-        """
+        
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size
 
-    def area(self):
-        """
-        Returns the area of the Square object.
-
-        Returns
-        -------
-        int
-            The area of the Square object.
-        """
-        return self.__size ** 2
+    def __str__(self):
+        
+        return f"[Square] {self.__width}"
