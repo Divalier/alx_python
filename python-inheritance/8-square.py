8b68c5549da022c99499319c6483ce03fa916a16
Divalier <111007588+Divalier@users.noreply.github.com>
2023-10-03 23:56:48 +0200

    up

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,59 +1,21 @@
-#!/usr/bin/env/python3
-"""
-Write a class Square that inherits from Rectangle (7-rectangle.py):
+#!/usr/bin/python3
+""" Inherits from rectangle """
+Rectangle = __import__('7-rectangle').Rectangle
 
-Instantiation with size: def __init__(self, size)::
-size must be private. No getter or setter
-size must be a positive integer, validated by integer_validator
-the area() method must be implemented
-
-"""
-class BaseGeometry:
-    """
-    A geometry class.
-    """
-
-    def area(self):
-       
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-class Rectangle(BaseGeometry):
-    """
-    A geometry class.
-    """
-
-    def __init__(self, width, height):
-        
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-       
-        return self.__width * self.__height
-
-    def __str__(self):
-        
-        return f"[Rectangle] {self.__width}/{self.__height}"
 
 class Square(Rectangle):
-    """
-    A geometry class.
-    """
-
+    """ A class to define a square using rectangle. """
     def __init__(self, size):
-        
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        """ initialize an size of rectangle"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ Returns the square of the rectangle """
+        return self.__size * self.__size
 
     def __str__(self):
-        
-        return f"[Square] {self.__width}"
+        """ Returns the print() and str() representation of rectangle """
+        string = "[" + str(self.__class__.__name__) + "]"
+        string += str(self.__size) + "/" + str(self.__size)
+        return string
