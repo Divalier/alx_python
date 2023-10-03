fd32abdfc9aefabcc46e3585fe766bd1fd7bff06
Divalier <111007588+Divalier@users.noreply.github.com>
2023-10-04 00:06:01 +0200

    g

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,45 +1,21 @@
-#!/usr/bin/env/python3
-"""
-Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py). (task based on 6-rectangle.py)
+#!/usr/bin/python3
+""" Inherits from BaseGeometry """
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-Instantiation with width and height: def __init__(self, width, height)::
-width and height must be private. No getter or setter
-width and height must be positive integers validated by integer_validator
-the area() method must be implemented
-print() should print, and str() should return, the following rectangle description: [Rectangle] <width>/<height>
-"""
-class BaseGeometry:
-    """
-    A geometry class.
-    """
-
-    def area(self):
-        
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
-    """
-    A geometry class.
-    """
-
+    """ A class to define a rectangle using base geometry. """
     def __init__(self, width, height):
-        
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """ initialize a new rectangle """
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
-
     def area(self):
-        
+        """ Returns the area of the rectangle """
         return self.__width * self.__height
-
     def __str__(self):
-        
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        """ Returns the print() and str() representation of rectangle """
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
