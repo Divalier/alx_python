afd2445e523a7f5a6c96cd50d2ce1fa3d27b9a0f
Divalier <111007588+Divalier@users.noreply.github.com>
2023-10-08 09:52:54 +0200

    Update 6-my_github.py

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,36 +1,24 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+"""
 import requests
 import sys
 
-def get_user_id(username, password):
-    """
-   Write a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
-
-You must use Basic Authentication with a personal access token as password to access to your information (only read:user permission is needed)
-The first argument will be your username
-The second argument will be your password (in your case, a personal access token as password)
-You must use the package requests and sys
-You are not allowed to import packages other than requests and sys
-You don’t need to check arguments passed to the script (number or type)
-    """
-    url = 'https://api.github.com/user'
-
-    response = requests.get(url, auth=(username, password))
-
-    if response.status_code != 200:
-        print(f"Error: {response.status_code}")
-        sys.exit(1)
-
-    return response.json()['id']
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Usage: python script.py <username> <password>")
-        sys.exit(1)
-
+def main():
     username = sys.argv[1]
-    password = sys.argv[2]
+    passw = sys.argv[2]
+    url = "https://api.github.com/user"
+    req = requests.get(url, auth=(username, passw))
 
-    user_id = get_user_id(username, password)
+    if req.status_code == 200:
+        res = req.json()
+        if res:
+            print(res['id'])
+    else:
+        print(None)
 
-    print(f"{user_id}")
+
+if __name__ == "__main__":
+    main()
