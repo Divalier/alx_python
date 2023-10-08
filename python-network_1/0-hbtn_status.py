cfadf4a9baf774dce38ba901ffe9c1fba1b6e0b6
Divalier <111007588+Divalier@users.noreply.github.com>
2023-10-08 09:57:22 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,6 @@
-#!/usr/bin/python3
-"""
-import request package
-"""
+"""importing requests"""
 import requests
-
-
-def main():
-    url = 'https://alu-intranet.hbtn.io/status'
-    req = requests.get(url)
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(req.text), req.text))
-"if chec main"
-
-if __name__ == "__main__":
-    main()
+request = requests.get("https://alu-intranet.hbtn.io/status")
+print("Body response:")
+print("\t- type:",type(request.reason) )
+print("\t- content:",request.reason)
