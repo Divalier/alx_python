bc94eb18d28304a847fece3bd594d679fec1ffda
Divalier <111007588+Divalier@users.noreply.github.com>
2023-10-08 09:55:52 +0200

    updated

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,27 +1,15 @@
+#!/usr/bin/python3
 """
-Write a Python script that fetches https://alu-intranet.hbtn.io/status
-
-You must use the package requests
-You are not allow to import packages other than requests
-The body of the response must be display like the following example (tabulation before -)"""
+import request package
+"""
 import requests
 
-def fetch_status():
-    """
-    Write a Python script that fetches https://alu-intranet.hbtn.io/status
 
-You must use the package requests
-You are not allow to import packages other than requests
-The body of the response must be display like the following example (tabulation before -)
-    """
-    url = 'https://intranet.hbtn.io/status'
-    response = requests.get(url)
+def main():
+    url = 'https://alu-intranet.hbtn.io/status'
+    req = requests.get(url)
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(req.text), req.text))
+"if chec main"
 
-    # If the response was successful, no Exception will be raised
-    response.raise_for_status()
-
-    body_str = f"Body response:\n \t- type: {type(response.text)}\n \t- content: {response.text}"
-    return body_str
-
-if __name__ == '__main__':
-    print(fetch_status())
+if __name__ == "__main__":
+    main()
--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
 Write a Python script that takes your GitHub credentials
-(username and password) and uses the GitHub API to display your id
 """
 import requests
 import sys
